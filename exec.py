682c11143cb5cf34d7a82433680ae21db422a820
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-05-21 09:50:23 -0300

    Posts on Facebook: DONE!

---
+++ exec.py
@@ -0,0 +1,48 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+from urllib2 import Request, urlopen
+from datetime import datetime, timedelta
+from threading import Thread
+import json
+import redis
+
+BASE_URL = "http://api.socialcron.com.br:5756/"
+
+log = file('dispatcher.log', 'a')
+redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+datetime_now = datetime.now().utcnow()
+one_minute_after = datetime_now - timedelta(minutes = 1)
+
+log.write('[DEBUG] - Dispatch executed at %s\n' %(datetime_now.strftime("%Y-%m-%dT%H:%M:%S%z")))
+
+schedules_keys = redis.keys('*')
+
+for key in schedules_keys:
+  schedule = json.loads(redis.get(key))
+  schedule_date = datetime.strptime(schedule['date'][0:-5], "%Y-%m-%dT%H:%M")
+
+  if schedule_date > datetime_now and schedule_date < one_minute_after:
+
+    photos_ids = []
+
+    if 'photos' in  schedule['post']:
+      for photo in schedule['post']['photos']:
+        fb_photo_data = urllib.urlencode({'url': BASE_URL + photo['path'], 'published': False, 'access_token': schedule['profile']['token']})
+        fb_photo_request = Request("https://graph.facebook.com/v2.4/me/photos")
+        fb_photo_response = json.loads(urlopen(fb_photo_request, data = fb_photo_data).read())
+        media_id = "{\"media_fbid\":\"%s\"}" %(fb_photo_response['id'])
+        photos_ids.append(media_id)
+
+    fb_post_data =  {'message': schedule['post']['content'], 'access_token': schedule['profile']['token']}
+    fb_post_request = Request("https://graph.facebook.com/v2.4/me/feed")
+
+    for index, id in enumerate(photos_ids):
+      fb_post_data["attached_media[%s]" %(index)] = id
+
+    fb_post_data =  urllib.urlencode(fb_post_data)
+    fb_post_response = json.loads(urlopen(fb_post_request, data = fb_post_data).read())
+
+  
--- sync.py
+++ sync.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from urllib2 import Request, urlopen
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
-import redis
 import json
+import redis
 
 BASE_URL = "http://api.socialcron.com.br:5756/"
 
 log = file('dispatcher.log', 'a')
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-datetime_now = datetime.now()
-now = datetime_now.strftime("%Y-%m-%d")
+datetime_now = datetime.now().utcnow()
+now = datetime_now.strftime("%Y-%m-%dT%H:%M") + "-0000"
+final = (datetime_now + timedelta(minutes=15)).strftime("%Y-%m-%dT%H:%M") + "-0000"
 
-log.write('[DEBUG] - Sync executed at %s\n' %(datetime_now.strftime("%Y-%m-%dT%H:%M:%S%z")))
+log.write('[DEBUG] - Sync executed at %s\n' %(now))
 
 auth_request = Request(BASE_URL + "oauth/token?username=root@root.com&password=root&grant_type=password")
 auth_request.add_header("Authorization", "Basic c29jaWFsY3Jvbjpzb2NpYWxjcm9u")
@@ -27,7 +28,7 @@ if auth_response['expires_in'] <= 120:
   refresh_request = json.loads(urlopen(refresh_request, data="").read())
   auth_response = refresh_request
 
-posts_request = Request(BASE_URL + "v2/schedules/day/" + now)
+posts_request = Request(BASE_URL + "v2/schedules/range/%s/%s" %(now, final))
 posts_request.add_header("Authorization", "Bearer " + auth_response['access_token'])
 posts_response = json.loads(urlopen(posts_request).read())
 
@@ -40,6 +41,15 @@ for post in posts_response:
  if diff > 60:
    try:
     if not redis.exists('schedule:%s' %(post['id'])):
+
+     photos_request = Request(BASE_URL + "v2/photos/post/" + str(post['post']['id']))
+     photos_request.add_header("Authorization", "Bearer " + auth_response['access_token'])
+     photos_response = json.loads(urlopen(photos_request).read())
+
+     for photos in photos_response:
+      post['post']['photos'] = []
+      post['post']['photos'].append(photos)
+
      redis.set('schedule:%s' %(post['id']), json.dumps(post), diff + 120)
      log.write("[DEBUG] - Schedule id %s stored. Will be posted at %s\n" %(post['id'], post['date']))
    except:
